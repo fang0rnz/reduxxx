ae6a9b6d7a2463e23703edfa6be01c52986c78ea
unknown <lucas.sartm@gmail.com>
2017-08-25 00:10:55 -0300

    first

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Single extends React.Component {
+    render() {
+        return (
+            <div className="single-photo">
+                i'm the single
+            </div>
+        )
+    }
+}
+
+export default Single;
